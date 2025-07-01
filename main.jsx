898ba91229ab331e89d61cef4d4d350117a30c99
reflexmills <lodog5378@gmail.com>
2025-07-02 03:09:14 +0700

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";      // файл стилей можем добавить позже
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
